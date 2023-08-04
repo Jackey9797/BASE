0ea6da4529c1fd4311ad0595e33448a6c516d1b1
fhyega <Jackey9797@outlook.com>
2023-08-04 14:50:18 +0800

    8.4 smart_KD

--- exp-a.sh
+++ exp-a.sh
@@ -1,6 +1,3 @@
-# python main.py --conf ECL-PatchTST --noise_rate 0.5 --idx -1 --device "cuda:1" --seed 2021 --data_name ETTh2 > expnew/1_0_noise5.log
-# python main.py --conf ECL-PatchTST --noise_rate 0.5 --idx -1 --device "cuda:1" --seed 2023 --data_name ETTh2  > expnew/3_0_noise5.log
-
-python main.py --conf ECL-PatchTST --noise_rate 0.5 --idx -1 --device "cuda:0" --seed 2021 --data_name ETTh2 --aligner 1 --same_init > expnew/1_4_si_noise5.log
-python main.py --conf ECL-PatchTST --noise_rate 0.5 --idx -1 --device "cuda:0" --seed 2022 --data_name ETTh2 --same_init > expnew/2_0_si_noise5.log
-python main.py --conf ECL-PatchTST --noise_rate 0.5 --idx -1 --device "cuda:0" --seed 2022 --data_name ETTh2 --aligner 1  > expnew/2_4_noise5.log
+python main.py --conf ECL-PatchTST --noise_rate 0.5 --idx -1 --data_name ETTh2 --device "cuda:0" --seed 33 --same_init   --aligner 1 --loss huber 
+# python main.py --conf ECL-PatchTST --noise_rate 0.5 --idx -1 --data_name ETTh2 --device "cuda:0" --seed 33 --same_init   --aligner 1 --loss huber  --always_align 0
+python main.py --conf ECL-PatchTST --pred_len 336 --noise_rate 0.5 --idx -1 --data_name ETTh2 --device "cuda:1" --seed 35 --same_init   --aligner 1 --loss huber
