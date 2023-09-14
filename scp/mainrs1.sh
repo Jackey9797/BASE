427892535c5cb0c13c3465fa378cacccebdb7248
fhyega <Jackey9797@outlook.com>
2023-09-14 09:08:25 -0400

    9.14 before change REF

--- scp/mainrs1.sh
+++ scp/mainrs1.sh
@@ -2,20 +2,27 @@
 #!/bin/bash
 
 
-models=("ECL-PatchTST")
-data_names=("weather")
+models=("ECL-Informer")
+data_names=("weather" "ETTm1")
 
 for model in "${models[@]}"
 do
   for data_name in "${data_names[@]}"
   do
-    for pred_len in 336
+    for pred_len in 96 192 336 720
     do 
       python -u main.py \
           --conf "$model" \
-          --noise_rate 0.5 \
+          --noise_rate 0.95 \
+          --n_heads 8 \
+          --d_model 128 \
+          --d_ff 512 \
+          --dropout 0.05 \
+          --fc_dropout 0.05 \
+          --e_layers 2 \
+          --seq_len 96 \
           --idx -1 \
-          --device "cuda:0" \
+          --device "cuda:1" \
           --aligner 1 \
           --loss huber \
           --refiner 1 \
@@ -29,9 +36,16 @@ do
 
       python -u main.py \
           --conf "$model" \
-          --noise_rate 0.5 \
+          --noise_rate 0.95 \
+          --n_heads 8 \
+          --d_model 128 \
+          --d_ff 512 \
+          --dropout 0.05 \
+          --fc_dropout 0.05 \
+          --e_layers 2 \
+          --seq_len 96 \
           --idx -1 \
-          --device "cuda:0" \
+          --device "cuda:1" \
           --aligner 0 \
           --loss mse \
           --refiner 0 \
@@ -46,7 +60,8 @@ do
   done
 done
 
-# data_names=( "electricity" "traffic")
+# data_names=("electricity" "traffic")
+
 # for model in "${models[@]}"
 # do
 #   for data_name in "${data_names[@]}"
@@ -55,9 +70,16 @@ done
 #     do 
 #       python -u main.py \
 #           --conf "$model" \
-#           --noise_rate 0.5 \
+#           --noise_rate 0.95 \
+#           --n_heads 8 \
+#           --d_model 512 \
+#           --d_ff 2048 \
+#           --dropout 0.05 \
+#           --fc_dropout 0.05 \
+#           --e_layers 2 \
+#           --seq_len 96 \
 #           --idx -1 \
-#           --device "cuda:0" \
+#           --device "cuda:1" \
 #           --aligner 1 \
 #           --loss huber \
 #           --refiner 1 \
@@ -66,15 +88,21 @@ done
 #           --train 1 \
 #           --data_name "$data_name" \
 #           --batch_size 8 \
-#           --early_break 10 \
 #           --mainrs 1 \
 #           > mainresult/"$model"'_OURS_'"$pred_len"'_'"$data_name"'.log'
 
 #       python -u main.py \
 #           --conf "$model" \
-#           --noise_rate 0.5 \
+#           --noise_rate 0.95 \
+#           --n_heads 8 \
+#           --d_model 512 \
+#           --d_ff 2048 \
+#           --dropout 0.05 \
+#           --fc_dropout 0.05 \
+#           --e_layers 2 \
+#           --seq_len 96 \
 #           --idx -1 \
-#           --device "cuda:0" \
+#           --device "cuda:1" \
 #           --aligner 0 \
 #           --loss mse \
 #           --refiner 0 \
@@ -83,10 +111,8 @@ done
 #           --train 1 \
 #           --data_name "$data_name" \
 #           --batch_size 8 \
-#           --early_break 10 \
 #           --mainrs 1 \
 #           > mainresult/"$model"'_ori_'"$pred_len"'_'"$data_name"'.log'
 #     done
 #   done
 # done
-
